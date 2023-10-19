6a65f7c6f402bd642313c4492338d0ee483a4756
Felipe Haddad <felipehaddad25@gmail.com>
2023-10-19 17:35:21 -0300

    add currency and account page, github and currency exchange apis

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 # from cs50 import SQL
 import sqlite3
+import requests
 
 # from flask_session import Session
 
@@ -64,37 +65,66 @@ def list():
     else:
         return redirect("/")
 
-# TODO
-@app.route("/add")
+
+@app.route("/add", methods=["POST", "GET"])
 def add():
 
-    session = {}
-    session["user_id"] = 1
-    session["user_name"] = "FelipeDH"
+    if request.method == "POST":
 
-    if session["user_id"]:
-        return render_template("add.html", session=session)
-    else:
-        return redirect("/")
-    # return render_template("add.html")
+        item_brand = request.form.get("brand")
+        item_name = request.form.get("name")
+        item_price = request.form.get("price")
+        item_infos = request.form.get("item_infos")
+        item_tax = request.form.get("has_tax")
+        item_link = request.form.get("item_link")
+
+        if not item_brand or not item_name or not item_price or not item_tax:
+            return render_template("message.html", message="Please fill all fileds with *")
+        else:
+            # add to the database
+            pass
+        return render_template("message.html", message="Item added")
+    else: # GET
+        return render_template("add.html")
 
 
 # TODO
-@app.route("/delete", methods=['POST'])
-def delete():
-    return render_template("error.html", error='item deleted')
-
-
-# TODO
-@app.route("/favorite", methods=["POST", "GET"])
+@app.route("/favorite", methods=["POST"])
 def favorite():
-    return render_template("error.html", error='item added to favorites')
 
+    item_id = int(request.form.get("item_id"))
+
+    if item_id == 1:
+
+        # ele já é favorito, então vamos desfavoritar
+        return render_template("message.html", message='Item will be defavorited')
+    elif item_id == 0:
+        # não é favorito, então vamos favoritar
+        return render_template("message.html", message='Item will be favorited')
+        
+    else:
+        # deu algum erro, informar na tela
+        return render_template("message.html", message="Internal error")
+
+    
+    # if not item_id:
+    #     return render_template("message.html", message="Item couldn't be favorited")
+    # else:
+    #     # chenge to one in the database the is_favorite from the selected item
+    #     pass
+    #     return render_template("message.html", message='item added to favorites')
 
 # TODO
-@app.route("/defavorite", methods=["POST", "GET"])
-def defavorite():
-    return render_template("error.html", error='item removed from favorites')
+@app.route("/delete", methods=["POST"])
+def delete():
+    item_id = request.form.get("item_id")
+    
+    if not item_id:
+        return render_template("message.html", message="Item couldn't be deleted")
+    else:
+        # delete form the database the selected item, with the id we got
+        pass
+        return render_template("message.html", message='item deleted')
 
 
 
@@ -112,12 +142,46 @@ def login():
 
 @app.route("/logout")
 def logout():
-    return redirect("/")
+    res = requests.get("http://economia.awesomeapi.com.br/json/last/USD-BRL")
+
+    # https://avatars.githubusercontent.com/u/90584577?v=4
+    # <Response [200]>
 
 
-@app.route("/account")
+    res = res.json()
+    # {'USDBRL': {'code': 'USD', 'codein': 'BRL', 'name': 'Dólar Americano/Real Brasileiro', 'high': '5.083', 'low': '5.0196', 'varBid': '-0.008', 'pctChange': '-0.16', 'bid': '5.0508', 'ask': '5.0515', 'timestamp': '1697743091', 'create_date': '2023-10-19 16:18:11'}}
+    
+    
+    res = res['USDBRL']['high']
+    # 5.083
+
+    return render_template("message.html")
+    # return redirect("/")
+
+
+@app.route("/account", methods=["POST", "GET"])
 def account():
-    return redirect("/")
+
+    if request.method == "POST":
+
+        currency = request.form.get("currency")
+        
+        # validate if currency exists
+        if currency not in CURRENCIES_ISO:
+            return render_template("message.html", message="Currency not valid, please choose one in the select field!")
+        else:
+            return render_template("message.html", message="Currency valid!")
+        
+    else: # GET
+        github_response = (requests.get("https://api.github.com/users/felipedh9")).json()
+        avatar_url = github_response['avatar_url']
+        if not avatar_url:
+            avatar_url = '#'
+
+        return render_template("account.html", avatar_url=avatar_url, currencies=CURRENCIES)
+    # return redirect("/")
+
+
 
 @app.route("/register", methods = ["POST", "GET"])
 def register():
@@ -136,9 +200,9 @@ def register():
         
         # validate if currency exists
         if currency not in CURRENCIES_ISO:
-            return render_template("error.html", error="Currency not valid, please choose one in the select field!")
+            return render_template("message.html", message="Currency not valid, please choose one in the select field!")
         else:
-            return render_template("error.html", error="Currency valid!")
+            return render_template("message.html", message="Currency valid!")
 
         
 
@@ -148,10 +212,43 @@ def register():
         return render_template("register.html", currencies=CURRENCIES)
     
     
+@app.route("/currency", methods = ["POST", "GET"])
+def currency():
+    # change currency in the database
+    if request.method == "POST":
 
+
+
+        return render_template("message.html", message="Currency changed")
+    
+
+    else: #GET
+        return render_template("currency.html", currencies=CURRENCIES)
+
+
+@app.route("/change", methods = ["POST"])
+def change():
+    # change  infos in the database for that user
+
+        return render_template("message.html", message="Infos changed")
 
 
 if __name__ == '__main__':
     app.run()
 
+
+
 # flask --app app.py --debug run
+
+# @app.route("/defavorite", methods=["POST"])
+# def defavorite():
+
+#     item_id = request.form.get("item_id")
+
+#     if not item_id:
+#         return render_template("message.html", message="Item couldn't be defavorite")
+#     else:
+#         # change is_Favorite to zero in database the selected item via its ID
+#         pass
+#         return render_template("message.html", message=f'{item_id} item defavorite')
+    
